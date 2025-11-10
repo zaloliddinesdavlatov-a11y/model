c61e8be80cf5dd588bbceb83b355b1797e83a04f
jaloliddin <zaloliddinesdavlatov@gmail.com>
2025-11-10 16:49:29 +0500

    first commit

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .models import BookModel
+
 
 
 urlpatterns = [
-    path('model/', BookModel,),
+
+    path('kitob/', views.hamma_kitoblar, name='hamma_kitoblar'),
     path('ber/', views.salom_beruvchi, name='salom_beruvchi'),
     path('mamlakat/', views.mamlakat, name='mamlakat'),
     path('mamlakat/viloyat/', views.viloyat, name='viloyat'),
--- django_project/views.py
+++ django_project/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import BookModel
+
+
+def hamma_kitoblar(request):
+    all_books = BookModel.objects.all()
+    context = {
+        'all_books':all_books
+    }
+    return render(request, 'asosiy_sahifa.html',context)
+
 
 def salom_beruvchi(request):
      return HttpResponse("Assalomu aleykum boy ota")
